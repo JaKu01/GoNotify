7ebfcebef5b7fdb12eb7e0d5ad02368be1faa86a
Jannes Kurth <>
2024-12-10 15:46:45 +0100

    Add icons

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,9 @@
 self.addEventListener('push', event => {
     const webPushData = event.data.json()
     const options = {
-        body: webPushData.body
+        body: webPushData.body,
+        icon: "https://notify.jskweb.de/apple-icon.png",
+        image: "https://notify.jskweb.de/apple-icon.png"
     };
     event.waitUntil(self.registration.showNotification(webPushData.subject, options));
 });
