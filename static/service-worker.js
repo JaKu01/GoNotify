e272aa2037de46a46b22692b9323a6aa4608b6c3
Jannes Kurth <>
2024-12-15 15:57:41 +0100

    Remove url

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,8 +2,6 @@ self.addEventListener('push', event => {
     const webPushData = event.data.json()
     const options = {
         body: webPushData.body,
-        icon: "https://notify.jskweb.de/apple-icon.png",
-        image: "https://notify.jskweb.de/apple-icon.png"
     };
     event.waitUntil(self.registration.showNotification(webPushData.subject, options));
 });
